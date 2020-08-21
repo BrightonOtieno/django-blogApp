6402efa08c01bb74d2a0f06cd7384eef6ae0d19b
brighton hoods <brightonhoods@gmail.com>
2020-08-21 20:44:17 +0300

    added post_detail_view

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, post_detail_view
 
 urlpatterns = [
     path('',home_page_view,name = "blog-home"),
+    path('<int:id>/detail/',post_detail_view,name = "post-detail"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -12,3 +12,13 @@ def home_page_view(request):
     }
 
     return render(request,"blog/index.html",context)
+
+# post detail view 
+def post_detail_view(request, id):
+    obj = get_object_or_404(Post, id= id)
+
+    context ={
+        "post":obj
+    }
+
+    return render(request,"blog/post_detail_page.html", context)
