d2937e5766691f7a28c558ac9dc95972c5d41c4a
brighton hoods <brightonhoods@gmail.com>
2020-08-24 21:31:40 +0300

    Finalizing

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
 
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html') ,name="password-reset-page"),
 
-    path('password-reset-done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') ,name="password-reset-done-page"),
+    path('password-reset-done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') ,name="password_reset_done"),
 
-    path('password-reset-confirm/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') ,name="password-reset-confirm-page"),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') ,name="password-reset-confirm-page"),
 
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html') ,name="password-reset-complete-page"),
 
