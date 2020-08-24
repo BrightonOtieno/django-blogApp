bc2c2e80e0de35046912f9bb1b790b7eda380f27
brighton hoods <brightonhoods@gmail.com>
2020-08-24 21:14:52 +0300

    Reset Password via Email Functionality

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -10,6 +10,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('profile/',user_views.user_profile_view ,name="user-profile"),
+
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html') ,name="password-reset-page"),
+
+    path('password-reset-done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') ,name="password-reset-done-page"),
+
+    path('password-reset-confirm/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') ,name="password-reset-confirm-page"),
+
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html') ,name="password-reset-complete-page"),
+
     path('registration/',user_views.user_registration_view , name="registration-page"),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html') , name="login-page"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout-page"),
